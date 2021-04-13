78331b7e855f8fc05d87bb464a0ad183eb7489ab
renequez <ecristianbujor@gmail.com>
2021-04-13 21:11:07 +0300

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+const About = () => {
+	return (
+		<>
+			<Head>
+				<title>Ninja List | About</title>
+				<meta name='keywords' content='ninjas' />
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde exercitationem
+					quas placeat magni quam eaque harum, enim aspernatur possimus ratione!
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde exercitationem
+					quas placeat magni quam eaque harum, enim aspernatur possimus ratione!
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import styles from '../../styles/Ninjas.module.css';
+
+export const getStaticProps = async () => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+	const data = await res.json();
+	return {
+		props: { ninjas: data },
+	};
+};
+
+const Ninjas = ({ ninjas }) => {
+	return (
+		<div>
+			<h1>All Ninjas</h1>
+			{ninjas.map((ninja, i) => {
+				return (
+					<Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+						<a className={styles.single}>
+							<h3>
+								{i + 1}. {ninja.name}
+							</h3>
+						</a>
+					</Link>
+				);
+			})}
+		</div>
+	);
+};
+
+export default Ninjas;
